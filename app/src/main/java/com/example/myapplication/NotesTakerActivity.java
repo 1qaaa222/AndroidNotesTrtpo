7bb401f69d95623115f23224b8ecd7a955a76415
Sciapan Hembar <102618764+Girraf-S@users.noreply.github.com>
2024-02-22 11:30:45 +0300

    Update NotesTakerActivity.java
    add onClickListener

--- app/src/main/java/com/example/myapplication/NotesTakerActivity.java
+++ app/src/main/java/com/example/myapplication/NotesTakerActivity.java
@@ -31,7 +31,23 @@ public class NotesTakerActivity extends AppCompatActivity {
         imageView_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String title= "";
+                String title= editText_title.getText().toString();
+                String description = editText_notes.getText().toString();
+                if(description.isEmpty()){
+                    Toast.makeText(NotesTakerActivity.this, "Please, enter description", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                SimpleDateFormat formatter = new SimpleDateFormat("EEE, d MMM YYYY HH:mm:ss");
+                Date date = new Date();
+                notes = new Notes();
+                notes.setTitle(title);
+                notes.setNotes(description);
+                notes.setDate(formatter.format(date));
+
+                Intent intent = new Intent();
+                intent.putExtra("notes", notes);
+                setResult(Activity.RESULT_OK.intent());
+                finish();
             }
         });
 
